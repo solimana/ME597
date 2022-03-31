ca1dbd32c91357382e6b898c80b7223225cb4bac
solimana <solimana@purdue.edu>
2022-03-31 19:50:34 -0400

    lab4

---
+++ lab_4_pkg/src/get_obstacle_map.py
@@ -0,0 +1,59 @@
+from PIL import Image, ImageOps 
+
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.cm as cm
+
+
+import yaml
+import pandas as pd
+
+from copy import copy, deepcopy
+import time
+
+class Map():
+    def __init__(self, map_name):
+        self.map_im, self.map_df, self.limits = self.__open_map(map_name)
+        self.image_array = self.__get_obstacle_map(self.map_im, self.map_df)
+    
+    def __repr__(self):
+        fig, ax = plt.subplots(dpi=150)
+        ax.imshow(self.image_array,extent=self.limits, cmap=cm.gray)
+        ax.plot()
+        return ""
+        
+    def __open_map(self,map_name):
+        # Open the YAML file which contains the map name and other
+        # configuration parameters
+        f = open(map_name + '.yaml', 'r')
+        map_df = pd.json_normalize(yaml.safe_load(f))
+        # Open the map image
+        map_name = map_df.image[0]
+        im = Image.open(map_name)
+        size = 200, 200
+        im.thumbnail(size)
+        im = ImageOps.grayscale(im)
+        # Get the limits of the map. This will help to display the map
+        # with the correct axis ticks.
+        xmin = map_df.origin[0][0]
+        xmax = map_df.origin[0][0] + im.size[0] * map_df.resolution[0]
+        ymin = map_df.origin[0][1]
+        ymax = map_df.origin[0][1] + im.size[1] * map_df.resolution[0]
+
+        return im, map_df, [xmin,xmax,ymin,ymax]
+
+    def __get_obstacle_map(self,map_im, map_df):
+        img_array = np.reshape(list(self.map_im.getdata()),(self.map_im.size[1],self.map_im.size[0]))
+        up_thresh = self.map_df.occupied_thresh[0]*255
+        low_thresh = self.map_df.free_thresh[0]*255
+
+        for j in range(self.map_im.size[0]):
+            for i in range(self.map_im.size[1]):
+                if img_array[i,j] > up_thresh:
+                    img_array[i,j] = 255
+                else:
+                    img_array[i,j] = 0
+        return img_array
+
+
+print(Map('my_map'))
